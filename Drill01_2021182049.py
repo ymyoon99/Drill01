52620ac81f6238424988baec66c654a608689dfa
ymyoon(OMEN) <ozone753@naver.com>
2023-09-14 15:21:05 +0900

    Update Drill01

--- Drill01_2021182049.py
+++ Drill01_2021182049.py
@@ -1,1 +1,33 @@
 import turtle
+
+def key_w():
+    turtle.setheading(90) #상
+    turtle.forward(50)
+    turtle.stamp()
+
+def key_a():
+    turtle.setheading(180) #좌
+    turtle.forward(50)
+    turtle.stamp()
+
+def key_s():
+    turtle.setheading(-90) #하
+    turtle.forward(50)
+    turtle.stamp()
+
+def key_d():
+    turtle.setheading(0) #우
+    turtle.forward(50)
+    turtle.stamp()
+
+def restart(): #ESC
+    turtle.reset()
+
+turtle.shape('turtle')
+
+turtle.onkey(key_w,'w')
+turtle.onkey(key_a,'a')
+turtle.onkey(key_s,'s')
+turtle.onkey(key_d,'d')
+turtle.onkey(restart,'Escape')
+turtle.listen()
